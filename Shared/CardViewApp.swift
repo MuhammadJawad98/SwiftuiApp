4f961c99fc2f5734486457858aee6c8e5b45600f
Muhamamd Jawad <m.jawad8991@gmail.com>
2022-01-11 18:07:50 +0500

    Initial Commit

---
+++ Shared/CardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardViewApp.swift
+//  Shared
+//
+//  Created by Ali Raza on 11/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
